7d08b435864ca0be2e9d95a706aa1c4704657bae
Turtle P.U <sokolov.p64@gmail.com>
2019-09-05 01:00:06 +0300

    making callback work

--- src/express.ts
+++ src/express.ts
@@ -7,18 +7,18 @@ export type StartOptions = {
 };
 
 export function callbackUrl(appUrl: string) {
-    return `https://${appUrl}/clb`;
+    return `https://${appUrl}/clb/`;
 };
 
 export function botUrl(appUrl: string) {
-    return `https://${appUrl}/bot`;
+    return `https://${appUrl}/bot/`;
 };
 
 export function startServer(options: StartOptions) {
     const app = Express();
     app.use(bodyParser.json());
-    app.post(`/bot`, options.botWebhook);
-    app.get(`/clb`, (req, _) => {
+    app.post(`/bot/`, options.botWebhook);
+    app.get(`/clb/`, (req, _) => {
         console.log(req.query);
     });
     return new Promise(resolve => app.listen(options.port, resolve));
