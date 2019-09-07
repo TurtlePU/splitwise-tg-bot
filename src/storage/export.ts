181f2332d1a7f7a02a5ccb94de9c2f04fd107ccd
Pavel Sokolov <turtlesokolov@yandex-team.ru>
2019-09-07 10:43:32 +0300

    debugging db proxy

--- src/storage/export.ts
+++ src/storage/export.ts
@@ -2,13 +2,13 @@ import Mongoose from 'mongoose';
 import TelegramBot from 'node-telegram-bot-api';
 
 const User = Mongoose.model('User', new Mongoose.Schema({
-    id: Number,
+    _id: Number,
     name: String,
     token: String
 }));
 
 function getName(user: TelegramBot.User) {
-    return user.username || user.first_name + (' ' + user.last_name || '');
+    return '@' + user.username || user.first_name + (' ' + user.last_name || '');
 }
 
 export function startStorage(mongoUri: string) {
@@ -16,6 +16,6 @@ export function startStorage(mongoUri: string) {
 }
 
 export function saveUser(user: TelegramBot.User, token: string) {
-    const userModel = new User({ id: user.id, name: getName(user), token });
+    const userModel = new User({ _id: user.id, name: getName(user), token });
     return userModel.save();
 }
