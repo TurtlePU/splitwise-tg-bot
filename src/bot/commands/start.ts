b7056f4057a17994ef434b7b778e65fe1c23ea37
Pavel Sokolov <turtlesokolov@yandex-team.ru>
2019-09-07 10:38:14 +0300

    minor fix

--- src/bot/commands/start.ts
+++ src/bot/commands/start.ts
@@ -2,19 +2,23 @@ import Locale from '@locale';
 
 import { Command } from './command';
 import { saveUser } from '@storage';
+import { AssertionError } from 'assert';
 
 const command: Command = {
     regexp: /\/start\b(?: (.*))?/,
     callback: ({ bot, authLink }) => async (msg, match) => {
+        if (!match) {
+            throw new AssertionError({ message: "Can't parse tokens" });
+        }
         if (msg.from) {
-            if (match) {
+            if (match[1]) {
                 await saveUser(msg.from, match[1]);
             }
             bot.sendMessage(
                 msg.chat.id,
                 Locale(msg.from.language_code).start.text(
                     msg.from.first_name,
-                    match ? undefined : authLink
+                    match[1] ? undefined : authLink
                 ),
                 { parse_mode: 'Markdown' }
             );
