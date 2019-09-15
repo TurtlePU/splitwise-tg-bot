efd51f10c602f8c0a9503f5de0f9f159ff102b22
Pavel Sokolov <turtlesokolov@yandex-team.ru>
2019-09-15 19:32:58 +0300

    fix empty balances

--- src/splitwise/api.ts
+++ src/splitwise/api.ts
@@ -11,9 +11,15 @@ export type Friend = User & {
 };
 
 export async function friends(token: string): Promise<Friend[]> {
-    return (await get('get_friends', token)).friends;
+    const friends: Friend[] = (await get('get_friends', token)).friends;
+    return friends.map(fix_empty_balances);
 };
 
 export async function me(token: string): Promise<User> {
     return (await get('get_current_user', token)).user;
 };
+
+function fix_empty_balances(friend: Friend): Friend {
+    const balance = friend.balance.filter(debt => debt.amount);
+    return { ...friend, balance };
+}
