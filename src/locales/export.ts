26454c9d6d7ee3b7c2e5cb581ee1b0facadadd47
Pavel Sokolov <turtlesokolov@yandex-team.ru>
2019-09-18 18:21:44 +0300

    Shielding fix

--- src/locales/export.ts
+++ src/locales/export.ts
@@ -12,5 +12,24 @@ export function getLocaledUi(localeCode = ''): Locale {
 }
 
 export function useAuthLink(link: string) {
-    locales.forEach(locale => locale.useAuthLink(link));
+    locales.forEach(locale => {
+        locale.useAuthLink(link);
+
+        const oldStart = locale.start.bind(locale);
+        locale.start = (name, authed) => oldStart(shield(name), authed);
+
+        const oldStats = locale.stats.bind(locale);
+        locale.stats = friends => {
+            const newFrends = friends.map(friend => {
+                return {
+                    ...friend, name: shield(friend.name)
+                };
+            });
+            return oldStats(newFrends);
+        };
+    });
+}
+
+function shield(str: string) {
+    return str.replace('_', '\\_');
 }
